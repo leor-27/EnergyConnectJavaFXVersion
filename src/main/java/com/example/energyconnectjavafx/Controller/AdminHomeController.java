79bcb63208396810b984faff05e5a57e549d6dd2
leor-27 <leoangelolarreagpoon@gmail.com>
2026-01-07 15:11:18 +0800

    Delete src/main directory

--- src/main/java/com/example/energyconnectjavafx/Controller/AdminHomeController.java
+++
@@ -1,4 +0,0 @@
-package com.example.energyconnectjavafx.Controller;
-
-public class AdminHomeController extends ProfileMainController {
-}
--- src/main/java/com/example/energyconnectjavafx/Controller/LandingPageController.java
+++
@@ -1,4 +0,0 @@
-package com.example.energyconnectjavafx.Controller;
-
-public class LandingPageController extends NavController {
-}
